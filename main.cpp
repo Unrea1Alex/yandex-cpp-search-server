b46c9bc97013548413cb2eb3467fdf54e3374bfa
Alexandr Vasilyev <ate1st1984@gmail.com>
2022-03-15 23:31:29 +0300

    1

--- main.cpp
+++ main.cpp
@@ -417,15 +417,15 @@ void MatchDocuments(const SearchServer& search_server, const std::string& query)
 
 int main()
 {
-	//TestSearchServer();
+	TestSearchServer();
 
     SearchServer search_server("и в на and with"s);
 
-    /*search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
+	search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
 	search_server.AddDocument(2, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2, 3});
 	search_server.AddDocument(3, "большой кот модный ошейник "s, DocumentStatus::ACTUAL, {1, 2, 8});
 	search_server.AddDocument(4, "большой пёс скворец евгений"s, DocumentStatus::ACTUAL, {1, 3, 2});
-    search_server.AddDocument(5, "большой пёс скворец василий"s, DocumentStatus::ACTUAL, {1, 1, 1});*/
+	search_server.AddDocument(5, "большой пёс скворец василий"s, DocumentStatus::ACTUAL, {1, 1, 1});
 
     AddDocument(search_server, 1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
 
